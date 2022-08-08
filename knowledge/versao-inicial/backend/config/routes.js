6b700b4e726b70ce70fb96130f6d4ebfff124df8
KennethOlusegun <88892775+KennethOlusegun@users.noreply.github.com>
2022-08-08 19:19:49 -0300

    chore: setting the vue toasted config

--- knowledge/versao-inicial/backend/config/routes.js
+++ knowledge/versao-inicial/backend/config/routes.js
@@ -7,55 +7,56 @@ module.exports = (app) => {
 
     app
         .route('/users')
-        // .all(app.config.passport.authenticate())
-        .post(app.api.user.save)
-        .get(app.api.user.get)
+        .all(app.config.passport.authenticate())
+        .post(admin(app.api.user.save))
+        .get(admin(app.api.user.get))
 
     app
         .route('/users/:id')
-        // .all(app.config.passport.authenticate())
-        .put(app.api.user.save)
-        .get(app.api.user.getById)
-        .delete(app.api.user.remove)
+        .all(app.config.passport.authenticate())
+        .put(admin(app.api.user.save))
+        .get(admin(app.api.user.getById))
+        .delete(admin(app.api.user.remove))
 
     app
         .route('/categories')
-        // .all(app.config.passport.authenticate())
-        .get(app.api.category.get)
-        .post(app.api.category.save)
+        .all(app.config.passport.authenticate())
+        .get(admin(app.api.category.get))
+        .post(admin(app.api.category.save))
 
+    // Cuidado com ordem! Tem que vir antes de /categories/:id
     app
         .route('/categories/tree')
-        // .all(app.config.passport.authenticate())
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getTree)
 
     app
         .route('/categories/:id')
-        // .all(app.config.passport.authenticate())
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getById)
-        .put(app.api.category.save)
-        .delete(app.api.category.remove)
+        .put(admin(app.api.category.save))
+        .delete(admin(app.api.category.remove))
 
     app
         .route('/articles')
-        // .all(app.config.passport.authenticate())
-        .get(app.api.article.get)
-        .post(app.api.article.save)
+        .all(app.config.passport.authenticate())
+        .get(admin(app.api.article.get))
+        .post(admin(app.api.article.save))
 
     app
         .route('/articles/:id')
-        // .all(app.config.passport.authenticate())
+        .all(app.config.passport.authenticate())
         .get(app.api.article.getById)
-        .put(app.api.article.save)
-        .delete(app.api.article.remove)
+        .put(admin(app.api.article.save))
+        .delete(admin(app.api.article.remove))
 
     app
         .route('/categories/:id/articles')
-        // .all(app.config.passport.authenticate())
+        .all(app.config.passport.authenticate())
         .get(app.api.article.getByCategory)
 
     app
         .route('/stats')
-        // .all(app.config.passport.authenticate())
+        .all(app.config.passport.authenticate())
         .get(app.api.stat.get)
 }
--- knowledge/versao-inicial/frontend/src/main.js
+++ knowledge/versao-inicial/frontend/src/main.js
@@ -10,7 +10,7 @@ import router from './config/router'
 Vue.config.productionTip = false
 
 require('axios').defaults.headers.common['Authorization'] =
-    'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Iktlbm5ldGggT2x1c2VndW4iLCJlbWFpbCI6Imtlbm5ldGhvbHVzZWd1bkBnbWFpbC5jb20iLCJhZG1pbiI6ZmFsc2UsImlhdCI6MTY1OTk0MDA1MywiZXhwIjoxNjYwMTk5MjUzfQ.OuwuxLaenj14nQ5gbjjMXiFvQQVMcKEyMV0sfioGHK4'
+    'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Iktlbm5ldGggT2x1c2VndW4iLCJlbWFpbCI6Imtlbm5ldGhvbHVzZWd1bkBnbWFpbC5jb20iLCJhZG1pbiI6ZmFsc2UsImlhdCI6MTY1OTk5NjI3MywiZXhwIjoxNjYwMjU1NDczfQ.iDO6O3jEJzufBtkzqJd9OQJk7paDxPjJPhdddL0_TY0'
 
 new Vue({
     store,
